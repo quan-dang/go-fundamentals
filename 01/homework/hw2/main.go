00598fcf01ec20af584714107ac59a02c4fbeee3
quan.dang1 <quan.dang1@onemount.com>
2021-09-04 08:48:50 +0700

    Rename folder name for hw

--- 01/homework/hw2/main.go
+++ 01/homework/hw2/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -11,69 +11,41 @@ import (
 
 func main() {
 	var (
-		err error
-		a   float64
-		b   float64
-		c   float64
+		err          error
+		x            int
+		randomNumber int
 	)
 
-	// input a, b, c from keyboard
-	a, err = readNumberFromKeyboard("a: ")
-	if err != nil {
-		fmt.Print(err.Error())
-		return
-	}
+	// generate the random number to guess
+	randomNumber = rand.Intn(100)
 
-	b, _ = readNumberFromKeyboard("b: ")
-	if err != nil {
-		fmt.Print(err.Error())
-		return
-	}
+	for x != -1 {
+		// input x from keyboard
+		x, err = readNumberFromKeyboard("Nhap x: ")
+		if err != nil {
+			fmt.Print(err.Error())
+			return
+		}
 
-	c, _ = readNumberFromKeyboard("c: ")
-	if err != nil {
-		fmt.Print(err.Error())
-		return
+		if x == randomNumber {
+			fmt.Println("Ban da doan dung!")
+			break
+		} else if x < randomNumber {
+			fmt.Println("So ban doan nho hon X!")
+		} else {
+			fmt.Println("So ban doan lon hon X!")
+		}
 	}
-
-	// solve quadractic equation
-	x1, x2 := solveQuadraticEquation(a, b, c)
-	fmt.Println("x1: ", x1)
-	fmt.Println("x2: ", x2)
 }
 
-// to solve quadratic equation
-func solveQuadraticEquation(a, b, c float64) (x1, x2 interface{}) {
-	delta := calculateDelta(a, b, c)
-	normalD := math.Sqrt(delta)
-	absoluteD := math.Sqrt(math.Abs(delta))
-
-	if normalD > 0 {
-		x1 = (-b + normalD) / (2 * a)
-		x2 = (-b - normalD) / (2 * a)
-	} else if absoluteD == 0 {
-		x1 = (-b) / (2 * a)
-		x2 = (-b) / (2 * a)
-	} else {
-		x1 = complex(-b, -absoluteD)
-		x2 = complex(-b, absoluteD)
-	}
-	return
-}
-
-//  to calculate delta for the quadratic equation
-func calculateDelta(a, b, c float64) float64 {
-	return math.Pow(b, 2) - 4*a*c
-}
-
-// read from keyboard and convert string to float
-func readNumberFromKeyboard(msg string) (result float64, err error) {
+// read from keyboard and convert string to int
+func readNumberFromKeyboard(msg string) (result int, err error) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print(msg)
 	str, _ := reader.ReadString('\n')
 	str = strings.TrimSuffix(str, "\n")
 
-	if result, err = strconv.ParseFloat(str, 64); err != nil {
+	if result, err = strconv.Atoi(str); err != nil {
 		return 0.0, err
 	}
 	return result, nil
--- 01/homework/hw3/main.go
+++ 01/homework/hw3/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -11,31 +11,54 @@ import (
 
 func main() {
 	var (
-		err          error
-		x            int
-		randomNumber int
+		err        error
+		n          int
+		primeSlice []int
 	)
 
-	// generate the random number to guess
-	randomNumber = rand.Intn(100)
+	// input N from keyboard
+	n, err = readNumberFromKeyboard("Nhap so tu nhien N: ")
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
 
-	for x != -1 {
-		// input x from keyboard
-		x, err = readNumberFromKeyboard("Nhap x: ")
-		if err != nil {
-			fmt.Print(err.Error())
-			return
-		}
+	// exit program if N < 0
+	if n < 0 || n > 100000 {
+		fmt.Println("N khong hop le!")
+		return
+	}
 
-		if x == randomNumber {
-			fmt.Println("Ban da doan dung!")
-			break
-		} else if x < randomNumber {
-			fmt.Println("So ban doan nho hon X!")
-		} else {
-			fmt.Println("So ban doan lon hon X!")
+	// loop over all natural number smaller than N+1
+	for i := 0; i < n+1; i++ {
+		if isPrime(i) {
+			primeSlice = append(primeSlice, i)
 		}
 	}
+
+	fmt.Println("Mang cac so nguyen to nho hon hoac bang N la: ", primeSlice)
+}
+
+// to check if a prime number
+func isPrime(inputNumber int) (result bool) {
+	prime_flag := 1
+
+	if inputNumber > 1 {
+		for i := 2; i < int(math.Sqrt(float64(inputNumber)))+1; i++ {
+			if inputNumber%i == 0 {
+				prime_flag = 0
+				break
+			}
+		}
+		if prime_flag == 0 {
+			result = false
+		} else {
+			result = true
+		}
+	} else {
+		result = false
+	}
+	return
 }
 
 // read from keyboard and convert string to int
